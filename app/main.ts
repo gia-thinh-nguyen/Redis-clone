82b5b52caf51dda24b293345bd61471da4da055e
gia-thinh-nguyen <gngu0011@student.monash.edu>
2024-06-01 05:22:09 +1000

    debug true

--- app/main.ts
+++ app/main.ts
@@ -7,9 +7,24 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
-  connection.on("data",async (data)=>{
-    console.log("Data received: ",data.toString());
-    connection.write("+PONG\r\n");
+  connection.on("data", (data:Buffer)=>{
+    console.log("Data received: ",data.toString().split("\r\n"));
+    const arr=data.toString().split("\r\n");
+    const method = arr[2]; // Convert method to a string
+    console.log("Method: ",method);
+    console.log("Data: ",arr[4]);
+    switch(method){
+      case "PING":
+        connection.write("+PONG\r\n");
+        break;
+      case "ECHO":
+        connection.write(`*${arr[4].length}\r\n$${data.toString().split("\r\n")[4]}\r\n`);
+        break;
+      default:
+        connection.write("-ERR unknown command\r\n");
+
+
+    }
   })
 });
 //
