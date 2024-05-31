ea86c4946f6271e0d09508440f4c1a624073b860
gia-thinh-nguyen <gngu0011@student.monash.edu>
2024-06-01 05:54:41 +1000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,22 +3,36 @@ import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
+const collection:{[key:string]:string}={};
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on("data", (data:Buffer)=>{
-    console.log("Data received: ",data.toString().split("\r\n"));
     const arr=data.toString().split("\r\n");
+    console.log("Data received: ",arr);
+    
     const method = arr[2]; // Convert method to a string
-    console.log("Method: ",method);
-    console.log("Data: ",arr[4]);
+    
     switch(method){
-      case "PING":
-        connection.write("+PONG\r\n");
-        break;
+        case "PING":
+          connection.write("+PONG\r\n");
+          break;
       case "ECHO":
-        connection.write(`$${arr[4].length}\r\n${data.toString().split("\r\n")[4]}\r\n`);
+        connection.write(`$${arr[4].length}\r\n${arr[4]}\r\n`);
+        break;
+      case "SET":
+        console.log(arr[4],arr[6])
+        connection.write("+OK\r\n");
+        collection[arr[4]]=arr[6];
+        break;
+      case "GET":
+        console.log(collection)
+        if(collection[arr[4]]){
+          connection.write(`$${collection[arr[4]].length}\r\n${collection[arr[4]]}\r\n`);
+        }
+        else{
+          connection.write(`$-1\r\n`)
+        }
         break;
       default:
         connection.write("-ERR unknown command\r\n");
