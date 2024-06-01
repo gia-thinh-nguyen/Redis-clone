0399555e530201a90bff67f5d0165d15d2f47613
gia-thinh-nguyen <gngu0011@student.monash.edu>
2024-06-02 02:50:29 +1000

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -64,7 +64,20 @@ if(argv.includes("--replicaof")){
   const [masterHost,masterPortString]=argv[argv.indexOf("--replicaof")+1].split(" ");
   const masterPort=parseInt(masterPortString);
   const handshake = net.connect({host: masterHost, port: masterPort}, () => {
-    handshake.write("*1\r\n$4\r\nPING\r\n");
+    const ping="*1\r\n$4\r\nPING\r\n";
+    const replconf1=`*2\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n${slavePort}\r\n`;
+    const replconf2=`*2\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n`
+    handshake.write(`${ping}`);
+    handshake.on("data",(data)=>{
+      console.log(data.toString())
+      console.log(data.toString()==="PONG")
+      if(data.toString()==="PONG"){
+        handshake.write(`*2\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n${slavePort}\r\n`)
+      }
+      if(data.toString()==="OK"){
+        handshake.write(`*2\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n`)
+      }
+    })
   });
   
 
